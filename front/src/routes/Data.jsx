0364719deb11da831cf7b04d2f25e5a0bd7dff25
Prithvi Reddy <u5021997@anu.edu.au>
2018-09-08 23:42:21 +1000

    Loading Screen

--- front/src/routes/Data.jsx
+++ front/src/routes/Data.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Table from '../components/Table'
 import {
+    Loader,
+    Dimmer,
     Input,
     Segment,
     Container
@@ -12,16 +14,21 @@ export default class Page extends Component {
     state = {
         dataLabels: [],
         dataTable: [],
-        request:''
+        request:'',
+        isLoading: false
     } 
 
     updateTable(){
-        fetch('http://localhost:5000/search',
+        const promise = fetch('http://localhost:5000/search',
             {method: 'POST', cache: 'default', headers:
             {'Accept': 'application/json', 'Content-Type': 'application/json'},
             body: JSON.stringify({'key': this.state.request})})
-            .then((response) => response.json())
-            .then((response) => this.setState(response))
+        this.setState({isLoading: true})
+        promise.then((response) => response.json())
+            .then((response) => {
+                this.setState(response) 
+                this.setState({isLoading: false})
+            })
     }
 
     render() {
@@ -37,22 +44,27 @@ export default class Page extends Component {
                 />
             )
         }
-
         return (
             <Container fluid style={{padding:'50px 50px'}}>
                 <h1>Duplicate Query and Explorer</h1>
-                <Segment>
-                    <h3>Enter Applicant Details</h3>
-                    <Input action={{ icon: 'search', onClick: e=>{this.updateTable()}}}
+                <Dimmer.Dimmable dimmed={this.state.isLoading} style={{height:'100%'}}>
+                    <Segment>
+                        <h3>Enter Applicant Details</h3>
+                        <Input action={{ icon: 'search',
+                            onClick: e=>{this.updateTable()}}}
                         placeholder='Search...' 
                         onChange={(e, {value}) => {this.setState({request: value})}}
-                    />
-                    <Container fluid style={{overflowX: 'scroll'}}>
-                        {table}
-                    </Container>
-                </Segment>
-
+                        />
+                        <Container fluid style={{overflowX: 'scroll', paddingTop: '10px'}}>
+                            {table}
+                        </Container>
+                    </Segment>
+                    <Dimmer inverted active={this.state.isLoading}>
+                        <Loader content="Loading"/>
+                    </Dimmer>
+                </Dimmer.Dimmable>
             </Container>
+
         )
     }
 }
