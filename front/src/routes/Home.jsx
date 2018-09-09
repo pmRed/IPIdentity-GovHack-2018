361f7e61f8dfd1381c8acde051f933886b9ae005
Prithvi Reddy <u5021997@anu.edu.au>
2018-09-09 15:42:15 +1000

    front

--- front/src/routes/Home.jsx
+++ front/src/routes/Home.jsx
@@ -14,12 +14,14 @@ export default class Page extends Component {
     render() {
         return (
             <Container>
-                <h1>Can we construct a user-friendly, fault-tolerant, distributed and immutable database for IP records?</h1> 
-                Our solution leverages blockchain technology to provide a decentralized database for serialising immutable artefacts of IP records, which satisfies the above requirements. Built on the EOSIO platform, IP-identity leverages the sophisticated smart-contract and on-chain persistence services provided by the EOSIO API.
-                
-                Our platform allows users to create and view IP documents through a web interface. Duplication of entries is intrinsically impossible through our use of document hashes as indices in the EOSIO Multi-Index DB API. Users receive feedback if their entry is similar to an existing entry through a poop-up list of similar entries.
-                
-                To facilitate rapid querying of our IP database, a custom persistence layer is created locally, which is verification against the blockchain. In this way, users get the best of both worlds: the responsiveness of a local databse, and the security of a decentralised blockchain database.
+                <Segment style={{ padding: '8em 0em' }} vertical>
+                    <h1>Can we construct a user-friendly, fault-tolerant, distributed and immutable database for IP records?</h1> 
+                    Our solution leverages blockchain technology to provide a decentralized database for serialising immutable artefacts of IP records, which satisfies the above requirements. Built on the EOSIO platform, IP-identity leverages the sophisticated smart-contract and on-chain persistence services provided by the EOSIO API.
+                    
+                    Our platform allows users to create and view IP documents through a web interface. Duplication of entries is intrinsically impossible through our use of document hashes as indices in the EOSIO Multi-Index DB API. Users receive feedback if their entry is similar to an existing entry through a poop-up list of similar entries.
+                    
+                    To facilitate rapid querying of our IP database, a custom persistence layer is created locally, which is verification against the blockchain. In this way, users get the best of both worlds: the responsiveness of a local databse, and the security of a decentralised blockchain database.
+                </Segment>
             </Container>
         )
     }
--- front/src/routes/Sources.jsx
+++ front/src/routes/Sources.jsx
@@ -10,29 +10,29 @@ export default class Page extends Component {
     render() {
         return (
             <Container>
-              <Segment style={{ padding: '30px 30px' }} vertical>
-                <h1> Data Sources and Discussion</h1>
+                <Segment style={{ padding: '30px 30px' }} vertical>
+                    <h1> Data Sources and Discussion</h1>
 
-                <h2>Can we construct a clean, fault-tolerent, distributed, borderless, IP ledger?</h2>
+                    <h2>Can we construct a clean, fault-tolerent, distributed, borderless, IP ledger?</h2>
 
-                <p>Yes we can! But wait, there\'s more! This decentralized database inherently will not allow duplicates. Such an application is mathematically invalid and will not succeed. With blockchain, we have solved the issue of duplication transparently and provided added benefits too. In addition, we have implemented a persistance layer between the blockchain and the user that allows the ability for any user to retrieve blockchain verified IP records from the their web browser.</p>
+                    <p>Yes we can! But wait, there\'s more! This decentralized database inherently will not allow duplicates. Such an application is mathematically invalid and will not succeed. With blockchain, we have solved the issue of duplication transparently and provided added benefits too. In addition, we have implemented a persistance layer between the blockchain and the user that allows the ability for any user to retrieve blockchain verified IP records from the their web browser.</p>
 
-                <List divided relaxed>
+                    <List divided relaxed>
 
-                  <List.Item>
-                    <List.Icon name='database' size='huge' verticalAlign='middle' />
-                    <List.Content>
-                      <List.Header as='a' href='https://data.gov.au/dataset/govhack-2018-ip-australia'>GovHack IP Australia 2018 Data set</List.Header>
-                      <List.Description>
-                        <p>We used this dataset to provide fuzzy searching and referencing against existing applicants that were serialized classically i.e. not on the blockchain where duplication is inherently solved and verification gauranteed.</p>
-                      </List.Description>
-                    </List.Content>
-                  </List.Item>
+                        <List.Item>
+                            <List.Icon name='database' size='huge' verticalAlign='middle' />
+                            <List.Content>
+                                <List.Header as='a' href='https://data.gov.au/dataset/govhack-2018-ip-australia'>GovHack IP Australia 2018 Data set</List.Header>
+                                <List.Description>
+                                    <p>We used this dataset to provide fuzzy searching and referencing against existing applicants that were serialized classically i.e. not on the blockchain where duplication is inherently solved and verification gauranteed.</p>
+                                </List.Description>
+                            </List.Content>
+                        </List.Item>
 
 
 
-                </List>
-              </Segment>
+                    </List>
+                </Segment>
             </Container>
         )
     }
